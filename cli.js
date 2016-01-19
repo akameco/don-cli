c465f19687a33847cefe2a88fa3449a8aab01fcb
akameco <akameco.t@gmail.com>
2016-01-20 00:58:48 +0900

    Add Open new article page

--- cli.js
+++ cli.js
@@ -5,10 +5,22 @@ const opn = require('opn');
 
 const cli = meow(`
   Usage
-    $ don open  -- open browser
-`);
+    $ don open  -- Open browser
+  Options
+    -n, --new  New article
+`, {
+  alias: {
+    n: 'new'
+  }
+});
 
-if(cli.input[0] === 'open') {
-  const donURL = 'http://staging.don.cps.im.dendai.ac.jp';
-  opn(donURL, {'wait': false});
+const baseURL = 'http://staging.don.cps.im.dendai.ac.jp';
+const opt = {wait: false};
+
+if (cli.input[0] === 'open') {
+  if (cli.flags.new) {
+    opn(`${baseURL}/articles/new`, opt);
+  } else {
+    opn(baseURL, opt);
+  }
 }
